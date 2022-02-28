b8b347af8254800391d5f2b3b64d89022ad697d3
Ali Bogatyrev <marksheyder06@gmail.com>
2022-02-28 19:11:55 +0300

    final3

--- yatube/posts/utils.py
+++ yatube/posts/utils.py
@@ -5,7 +5,8 @@ def obj_in_page(request):
     return 10
 
 
-def posts_paginator(obj, count, page_number):
+def posts_paginator(request, obj, count):
+    page_number = request.GET.get('page')
     paginator = Paginator(obj, count)
     page_obj = paginator.get_page(page_number)
     return page_obj
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,7 +1,8 @@
+from multiprocessing import context
+from re import template
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.conf import settings
-
 
 from .models import Post, Group, User
 from .forms import PostForm
@@ -11,8 +12,7 @@ from .utils import posts_paginator
 def index(request):
     template = 'posts/index.html'
     posts = Post.objects.all()
-    page_number = request.GET.get('page')
-    page_obj = posts_paginator(posts, settings.OBJ_IN_PAGE, page_number)
+    page_obj = posts_paginator(request, posts, settings.OBJ_IN_PAGE)
     context = {
         'page_obj': page_obj,
     }
@@ -23,8 +23,7 @@ def group_posts(request, slug):
     template = 'posts/group_list.html'
     group = get_object_or_404(Group, slug=slug)
     posts = group.group_posts.all()
-    page_number = request.GET.get('page')
-    page_obj = posts_paginator(posts, settings.OBJ_IN_PAGE, page_number)
+    page_obj = posts_paginator(request, posts, settings.OBJ_IN_PAGE)
     context = {
         'group': group,
         'page_obj': page_obj,
@@ -37,8 +36,7 @@ def profile(request, username):
     template = 'posts/profile.html'
     posts = user.author_posts.all()
     posts_count = posts.count()
-    page_number = request.GET.get('page')
-    page_obj = posts_paginator(posts, settings.OBJ_IN_PAGE, page_number)
+    page_obj = posts_paginator(request, posts, settings.OBJ_IN_PAGE)
     context = {
         'page_obj': page_obj,
         'username': user,
@@ -59,12 +57,17 @@ def post_detail(request, post_id):
 @login_required
 def post_create(request):
     form = PostForm(request.POST or None)
+    template = 'posts/create_post.html'
     if form.is_valid():
         post = form.save(commit=False)
         post.author = request.user
         post.save()
         return redirect('posts:profile', username=request.user)
-    return render(request, 'posts/create_post.html', {'form': form})
+    context = {
+        'form': form,
+        'is_edit': False,
+    }
+    return render(request, template, context)
 
 
 @login_required
