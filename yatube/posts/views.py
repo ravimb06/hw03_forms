62a99bf5ab46a45f5ce6352c9f25f8cf06e13b86
Ali Bogatyrev <marksheyder06@gmail.com>
2022-02-28 19:19:48 +0300

    final3

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,5 +1,3 @@
-from multiprocessing import context
-from re import template
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.conf import settings
